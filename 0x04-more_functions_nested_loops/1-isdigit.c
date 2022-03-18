b0f70d890f943e1ef725ca7d9a52f3615dc1fe03
Harold TCHEGNON <siharold.tchegnon@gmail.com>
2022-03-17 20:34:44 -0700

    ok

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,11 +6,12 @@
  *Return: 1 if c is a digit
  *0 otherwise.
 */
-int main(int c) {
+int _isdigit(int c) {
 	if ((c >= '0') && (c <= '9'))
 	{
 		return (1);
-	} else {
+	} else 
+	{
 		return (0);
 	}
 }
